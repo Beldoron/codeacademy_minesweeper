9e3b8f89baf8ce8d48690038410283ede949988d
Michael <michael.toepfer187@gmail.com>
2018-04-26 14:04:04 +0200

    check for bomb tiles and flipTile

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -24,11 +24,45 @@ const generateBombBoard = (numberOfRows, numberOfColums, numberOfBombs) => {
   while(numberOfBombsPlaced < numberOfBombs ) { //bombs can be placed above bombs, fixable with control flow //
     let randomRowIndex = Math.floor(Math.random() * numberOfRows);
     let randomColumIndex = Math.floor(Math.random() * numberOfColums);
-    board[randomRowIndex][randomColumIndex] = "B";
-    numberOfBombsPlaced++;
+    if (board[randomRowIndex][randomColumIndex] !== "B"){
+      board[randomRowIndex][randomColumIndex] = "B";
+      numberOfBombsPlaced++;
+    }
+
   }
   return board;
 };
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [[-1,-1], [-1, 0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+    if (neighborRowIndex >= 0 && neighborRowIndex <=numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex <=numberOfColumns) {
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] === "B"){
+        numberOfBombs++;
+      };
+    };
+  });
+  return numberOfBombs;
+} ;
+
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  if (playerBoard[rowIndex][columnIndex] !== " "){
+    console.log("This tile has already been flipped");
+    return;
+  } else if (bombBoard[rowIndex][columnIndex] === "B") {
+    playerBoard[rowIndex][columnIndex] = "B"
+  } else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+
+  };
+}
+
 
 const printBoard = (board) => {
   console.log(board.map(row => row.join(" | ")).join("\n"));
@@ -42,3 +76,7 @@ printBoard(playerBoard);
 
 console.log("Bomb Board:")
 printBoard(bombBoard);
+
+flipTile(playerBoard, bombBoard, 1, 0)
+console.log("Updated Player Board:")
+printBoard(playerBoard);
